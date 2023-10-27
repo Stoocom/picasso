13e82b350c68f440ca77df4ebec26f467e554c30
a.eliseev <a.eliseev@demiusgroup.ru>
2023-10-27 09:23:34 +0300

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { createBrowserHistory } from "history";
 import Main from '../pages/Main.js';
 import OnePost from '../pages/OnePost.js';
-
-const hist = createBrowserHistory();
+import { useEffect } from 'react';
 
 function App() {
+
+    useEffect(() => {
+        if(window.location.hash.indexOf("#%") > -1 ) { 
+           window.location.hash = ""; 
+        }
+     });
+
   return (
-    <HashRouter history={hist}>
+    <HashRouter>
         <Routes>
             <Route path="/" element={<Main />}/>
             <Route path="post/:id" element={<OnePost />} />

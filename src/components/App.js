85d01c95d6b41446aac19c752e2f55b5c54df465
a.eliseev <a.eliseev@demiusgroup.ru>
2023-10-27 09:38:50 +0300

    added 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router , Routes, Route } from 'react-router-dom';
 import Main from '../pages/Main.js';
 import OnePost from '../pages/OnePost.js';
 
 function App() {
   return (
-    <HashRouter>
+    <Router basename="/">
         <Routes>
-            <Route path="/" element={<Main />}/>
+            <Route path="" element={<Main />}/>
             <Route path="post/:id" element={<OnePost />} />
         </Routes>
-    </HashRouter>
+    </Router>
         
   );
 }

3c0d5e89314c7752cb9665ed2fe76940f6fda1fc
a.eliseev <a.eliseev@demiusgroup.ru>
2023-10-27 08:43:40 +0300

    fixed history

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserHistory } from "history";
 import Main from '../pages/Main.js';
 import OnePost from '../pages/OnePost.js';
-// const baseUrl = document.getElementsByTagName("base") 
-// [0].getAttribute("href");
+
+const hist = createBrowserHistory();
 
 function App() {
   return (
-    <Router basename={"/"}>
+    <HashRouter history={hist}>
         <Routes>
             <Route path="/" element={<Main />}/>
             <Route path="post/:id" element={<OnePost />} />
         </Routes>
-    </Router>
+    </HashRouter>
         
   );
 }
